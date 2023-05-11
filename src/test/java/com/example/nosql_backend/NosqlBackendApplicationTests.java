891b8449e1d889727df7131a371bc8dfe96c3ec5
umair beig <umairbeig@gmail.com>
2023-05-11 18:39:42 +0530

    back

---
+++ src/test/java/com/example/nosql_backend/NosqlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nosql_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
