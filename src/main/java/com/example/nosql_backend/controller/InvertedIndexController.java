8038141a47b6d67aa86eb8aed6cb84d552870323
umair beig <umairbeig@gmail.com>
2023-05-14 20:46:57 +0530

    mongo db+get doc array for word

--- src/main/java/com/example/nosql_backend/controller/InvertedIndexController.java
+++ src/main/java/com/example/nosql_backend/controller/InvertedIndexController.java
@@ -1,18 +1,33 @@
 package com.example.nosql_backend.controller;
-
+import com.example.nosql_backend.entity.invertedindex;
+import com.example.nosql_backend.repo.InvertedIndexRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 
+@RestController
+@RequestMapping("/api")
 public class InvertedIndexController {
-    @GetMapping("/inverted-index")
-    public String getInvertedIndex(@RequestParam String word) {
-        // Implement the logic to retrieve the inverted index based on the given word
-        // Return the inverted index as the response
-        System.out.println("ok");
-        return "umair";
+
+    @Autowired
+    private InvertedIndexRepository invertedIndexRepository;
+
+    @GetMapping("/search/{word}")
+    public ResponseEntity<invertedindex.Doc[]> searchWord(@PathVariable String word) {
+        System.out.println("the entered word is:" +word);
+        invertedindex invertedIndex = invertedIndexRepository.findByWord(word);
+
+
+        if (invertedIndex != null) {
+            invertedindex.Doc[] docs = invertedIndex.getDocs();
+//            System.out.println(docs);
+            return ResponseEntity.ok().body(docs);
+        }
+        System.out.println("not found");
+        return ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/com/example/nosql_backend/entity/invertedindex.java
@@ -0,0 +1,78 @@
+package com.example.nosql_backend.entity;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "indextable")
+public class invertedindex  {
+    @Id
+    private String id;
+
+    private String word;
+    private Doc[] docs;
+
+    public invertedindex(String id, String word, Doc[] docs) {
+        this.id = id;
+        this.word = word;
+        this.docs = docs;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public Doc[] getDocs() {
+        return docs;
+    }
+
+    public void setDocs(Doc[] docs) {
+        this.docs = docs;
+    }
+
+    // Constructor, getters, and setters
+    // ...
+
+    public static class Doc {
+        private String filename;
+        private int count;
+
+        public Doc(String filename, int count) {
+            this.filename = filename;
+            this.count = count;
+        }
+
+        public String getFilename() {
+            return filename;
+        }
+
+        public void setFilename(String filename) {
+            this.filename = filename;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        // Constructor, getters, and setters
+        // ...
+    }
+}
+
+
+
+
